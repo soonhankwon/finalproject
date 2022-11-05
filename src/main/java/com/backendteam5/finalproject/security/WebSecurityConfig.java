e07ac4d8b38148b881ab763d6c3689cb7dc0f088
Sun1203 <84012715+Sun1203@users.noreply.github.com>
2022-11-05 18:10:56 +0900

    Feat : Courier test데이터 생성.

--- src/main/java/com/backendteam5/finalproject/security/WebSecurityConfig.java
+++ src/main/java/com/backendteam5/finalproject/security/WebSecurityConfig.java
@@ -39,6 +39,7 @@ public class WebSecurityConfig {
                         .antMatchers("/css/**").permitAll()
                         // 회원 관리 처리 API 전부를 login 없이 허용
                         .antMatchers("/user/**").permitAll()
+                        .antMatchers("/test").permitAll()
                         // 어떤 요청이든 '인증'
                         .anyRequest().authenticated()
                 )

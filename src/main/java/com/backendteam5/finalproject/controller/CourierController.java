34767e8370cfb1313e545724b4a603271f1864c2
soonable@naver.com <soonable@naver.com>
2022-11-08 14:46:18 +0900

    feat #4 PATCH API 기본 세팅 & 서비스로직 구현중

--- src/main/java/com/backendteam5/finalproject/controller/CourierController.java
+++ src/main/java/com/backendteam5/finalproject/controller/CourierController.java
@@ -1,8 +1,14 @@
 package com.backendteam5.finalproject.controller;
 
 
+import com.backendteam5.finalproject.dto.CourierReqUpdateDto;
+import com.backendteam5.finalproject.dto.CourierResUpdateDto;
+import com.backendteam5.finalproject.security.UserDetailsImpl;
 import com.backendteam5.finalproject.service.CourierService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,4 +22,23 @@ public class CourierController {
     public void dummieTest() {
         courierService.createDommie();
     }
+    @PatchMapping("/api/post/{username}")
+    public CourierResUpdateDto updateCourier(@PathVariable String username,
+                                             @AuthenticationPrincipal UserDetailsImpl userDetails,
+                                             CourierReqUpdateDto courierReqUpdateDto) {
+        return courierService.updateCourier(username, userDetails, courierReqUpdateDto);
+    }
+    @PatchMapping("/api/post/{courierId}/check")
+    public CourierResUpdateDto checkCourierState(@PathVariable Long courierId,
+                                                 @AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                 CourierReqUpdateDto courierReqUpdateDto) {
+        return courierService.checkCourierState(courierId, userDetails, courierReqUpdateDto);
+    }
+    @PatchMapping("/api/post/{courierId}/uncheck")
+    public CourierResUpdateDto uncheckCourierState(@PathVariable Long courierId,
+                                                   @AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                   CourierReqUpdateDto courierReqUpdateDto) {
+        return courierService.uncheckCourierState(courierId, userDetails, courierReqUpdateDto);
+    }
+
 }
--- src/main/java/com/backendteam5/finalproject/service/CourierService.java
+++ src/main/java/com/backendteam5/finalproject/service/CourierService.java
@@ -1,8 +1,13 @@
 package com.backendteam5.finalproject.service;
 
 
+import com.backendteam5.finalproject.dto.CourierReqUpdateDto;
+import com.backendteam5.finalproject.dto.CourierResUpdateDto;
+import com.backendteam5.finalproject.entity.Account;
 import com.backendteam5.finalproject.entity.Courier;
+import com.backendteam5.finalproject.repository.AccountRepository;
 import com.backendteam5.finalproject.repository.CourierRepository;
+import com.backendteam5.finalproject.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +20,7 @@ import java.util.Date;
 public class CourierService {
 
     private final CourierRepository courierRepository;
+    private final AccountRepository accountRepository;
 
     @Transactional
     public void createDommie() {
@@ -30,17 +36,43 @@ public class CourierService {
         String username = "";
 
 
-        for (int i=0;i<=20;i++) {
+        for (int i = 0; i <= 20; i++) {
             String index = Integer.toString(i);
 
             Courier courier = new Courier(area + index, state, customer + index, arrivalDate, username);
             courierRepository.save(courier);
         }
+    }
 
+    @Transactional
+    public CourierResUpdateDto updateCourier(String username, UserDetailsImpl userDetails,
+                                             CourierReqUpdateDto courierReqUpdateDto) {
+        Account account = accountRepository.findByUsername(username)
+                .orElseThrow(() -> new NullPointerException("등록되어있지 않은 택배기사입니다"));
 
+        Courier courier = new Courier(courierReqUpdateDto.getArea(), courierReqUpdateDto.getState(),
+                courierReqUpdateDto.getCustomer(), courierReqUpdateDto.getArrivalDate(), courierReqUpdateDto.getUsername());
 
+        courierRepository.save(courier);
+        return new CourierResUpdateDto();
+    }
 
+    public CourierResUpdateDto checkCourierState(Long courierId, UserDetailsImpl userDetails,
+                                                 CourierReqUpdateDto courierReqUpdateDto) {
 
+        Courier courier = courierRepository.findById(courierId)
+                .orElseThrow(() -> new NullPointerException("해당 운송장이 존재하지 않습니다"));
 
+        courierRepository.save(courier);
+        return new CourierResUpdateDto();
+    }
+
+    public CourierResUpdateDto uncheckCourierState(Long courierId, UserDetailsImpl userDetails,
+                                                   CourierReqUpdateDto courierReqUpdateDto) {
+        Courier courier = courierRepository.findById(courierId)
+                .orElseThrow(() -> new NullPointerException("해당 운송장이 존재하지 않습니다"));
+
+        courierRepository.save(courier);
+        return new CourierResUpdateDto();
     }
 }

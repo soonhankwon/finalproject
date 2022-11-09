53a5ba42776aa9c903857cc433b09f42951de75b
GGuiGui <rbqjachl95@gmail.com>
2022-11-09 16:29:36 +0900

    Chore : AdminMainResDto 어노테이션 제거 #8

--- src/main/java/com/backendteam5/finalproject/dto/AdminMainResDto.java
+++ src/main/java/com/backendteam5/finalproject/dto/AdminMainResDto.java
@@ -2,7 +2,6 @@ package com.backendteam5.finalproject.dto;
 
 import com.backendteam5.finalproject.entity.Account;
 import com.backendteam5.finalproject.entity.Courier;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,9 +12,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 public class AdminMainResDto {
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     private List<Account> userList;
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     private List<Courier> courierList;
 
     public AdminMainResDto(List<Account> userList, List<Courier> courierList){

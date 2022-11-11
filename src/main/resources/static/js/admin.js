4051dd4d4a3f31ba1fa342d2ae371b7bede58cd9
GGuiGui <rbqjachl95@gmail.com>
2022-11-12 05:20:44 +0900

    Fix : 초기화면 admin과 택배기사만 나오게 수정 및 url 길이에 대한 갯수 제한 알림 #12

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -26,32 +26,32 @@ function searchAll(){
                     userTable.append(html);
                 }
             }
-
-            if(courierList.length === 0){
-                courierTable.append("<tr><td colspan='8'>검색된 정보가 없습니다.</td></tr>")
-            }else{
-                for(let i=0; i<courierList.length; i++){
-
-                    let id = courierList[i]['id'];
-                    let route = courierList[i]['route'];
-                    let subroute = courierList[i]['subRoute'];
-                    let customer = courierList[i]['customer'];
-                    let realstate = courierList[i]['state'];
-                    let arrivalDate = courierList[i]['arrivalDate'];
-                    let trueusername = courierList[i]['username'];
-
-                    let html = "<tr>"+
-                        "<td><input type='checkbox' name='Courier-select'></td>"+
-                        `<td onclick="updateOne(${i})" align='middle'><input id='number-${i}' name='number-${i}' type='text' value='${id}'readonly/>`+"</td>"+
-                        `<td align='middle'><input id='route-${i}' name='route-${i}' type='text' value='${route}'readonly/>`+"</td>"+
-                        `<td align='middle'><input id='subroute-${i}' name='subroute-${i}' type='text' value='${subroute}'readonly/>`+"</td>"+
-                        `<td align='middle'><input id='state-${i}' name='state-${i}' type='text' value='${realstate}'readonly/>`+"</td>"+
-                        `<td align='middle'><input id='customer-${i}' name='customer-${i}' type='text' value='${customer}'readonly/>`+"</td>"+
-                        `<td align='middle'><input id='arrivalDate-${i}' name='arrivalDate-${i}' type='text' value='${arrivalDate}'readonly/>`+"</td>"+
-                        `<td align='middle'><input id='username-${i}' name='username-${i}' type='text' value='${trueusername}'readonly/>`+"</td></tr>"
-                    courierTable.append(html);
-                }
-            }
+            courierTable.append("<tr><td colspan='8'>검색된 정보가 없습니다.</td></tr>")
+            // if(courierList.length === 0){
+            //     courierTable.append("<tr><td colspan='8'>검색된 정보가 없습니다.</td></tr>")
+            // }else{
+            //     for(let i=0; i<courierList.length; i++){
+            //
+            //         let id = courierList[i]['id'];
+            //         let route = courierList[i]['route'];
+            //         let subroute = courierList[i]['subRoute'];
+            //         let customer = courierList[i]['customer'];
+            //         let realstate = courierList[i]['state'];
+            //         let arrivalDate = courierList[i]['arrivalDate'];
+            //         let trueusername = courierList[i]['username'];
+            //
+            //         let html = "<tr>"+
+            //             "<td><input type='checkbox' name='Courier-select'></td>"+
+            //             `<td onclick="updateOne(${i})" align='middle'><input id='number-${i}' name='number-${i}' type='text' value='${id}'readonly/>`+"</td>"+
+            //             `<td align='middle'><input id='route-${i}' name='route-${i}' type='text' value='${route}'readonly/>`+"</td>"+
+            //             `<td align='middle'><input id='subroute-${i}' name='subroute-${i}' type='text' value='${subroute}'readonly/>`+"</td>"+
+            //             `<td align='middle'><input id='state-${i}' name='state-${i}' type='text' value='${realstate}'readonly/>`+"</td>"+
+            //             `<td align='middle'><input id='customer-${i}' name='customer-${i}' type='text' value='${customer}'readonly/>`+"</td>"+
+            //             `<td align='middle'><input id='arrivalDate-${i}' name='arrivalDate-${i}' type='text' value='${arrivalDate}'readonly/>`+"</td>"+
+            //             `<td align='middle'><input id='username-${i}' name='username-${i}' type='text' value='${trueusername}'readonly/>`+"</td></tr>"
+            //         courierTable.append(html);
+            //     }
+            // }
         }
     })
 }
@@ -129,13 +129,16 @@ function searchCourier(){
 
 /* 다대다 할당 */
 function updateCourier(){
+    alert("입력된 값의 유효성을 검사합니다.");
     let usernames = [];
+    let usernamelenth = 0;
     let courierIds = [];
     let checkbox = $("input:checkbox[name=User-select]:checked");
     checkbox.each(function (i){
         let tr = checkbox.parent().parent().eq(i);
         let td = tr.children();
         usernames.push(td.eq(2).text());
+        usernamelenth += td.eq(2).text().length;
     })
     if(usernames.length <1){
         alert("유저를 한명만 선택하세요");
@@ -152,8 +155,17 @@ function updateCourier(){
     if(usernames.length !== 1 && usernames.length !== courierIds.length){
         alert("user 다중 선택시 운송장 갯수와 같아야 합니다.");
         return;
+    } else if(usernames.length===1 && usernames[0].length + courierIds.length > 2080){
+        alert("courier의 갯수가 너무 많습니다.");
+        return;
+    } else if(usernamelenth + courierIds.length > 2080){
+        alert("username의 문자 총 길이가 너무 많습니다.");
+        return;
     }
-
+    if(!confirm("작업을 수행하시겟습니까?")){
+        alert("작업 취소");
+        return;
+    }
     let Params = '?usernames='+usernames+"&courierIds="+courierIds;
     $.ajax({
         type: 'PATCH',

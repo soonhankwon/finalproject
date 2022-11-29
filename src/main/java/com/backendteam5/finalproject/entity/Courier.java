7c63f3312d0e3555fce154aebfd4a2e62ccba3df
soonable@naver.com <soonable@naver.com>
2022-11-29 16:11:45 +0900

    #27 feat: new field

--- src/main/java/com/backendteam5/finalproject/entity/Courier.java
+++ src/main/java/com/backendteam5/finalproject/entity/Courier.java
@@ -16,28 +16,30 @@ public class Courier {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String route;
-    private int subRoute;
-    private Boolean state;
+//    private String area;
+//    private String route;
+//    private String subRoute;
+    private String state;
     private String customer;
+    private String registerDate;
     private String arrivalDate;
-    private String username;
 
-    public Courier(String route, int subRoute, Boolean state, String customer, String arrivalDate, String username) {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "username_id", nullable = false)
+    private DeliveryAssignment deliveryAssignment;
+
+    public Courier(String route, int subRoute, Boolean state, String customer, String arrivalDate, DeliveryAssignment deliveryAssignment) {
         this.route = route;
         this.subRoute = subRoute;
         this.state = state;
         this.customer = customer;
         this.arrivalDate = arrivalDate;
-        this.username = username;
+        this.deliveryAssignment = deliveryAssignment;
     }
 
     public void update(CourierReqUpdateDto courierReqUpdateDto) {
         this.state = courierReqUpdateDto.getState();
         this.arrivalDate = courierReqUpdateDto.getArrivalDate();
-        this.username = courierReqUpdateDto.getUsername();
-    }
-    public void setUpdate(int j, String username) {
-        this.username = username;
+        this.deliveryAssignment = courierReqUpdateDto.getDeliveryAssignment();
     }
 }

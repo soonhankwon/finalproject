d5428ebcc78b45995c764ecefcac754bb418e457
soonable@naver.com <soonable@naver.com>
2023-02-08 21:36:23 +0900

    refactor : 코드 결합도를 낮춤 & 메서드 세분화

--- src/main/java/com/backendteam5/finalproject/service/AccountService.java
+++ src/main/java/com/backendteam5/finalproject/service/AccountService.java
@@ -2,50 +2,50 @@ package com.backendteam5.finalproject.service;
 
 import com.backendteam5.finalproject.dto.SignupRequestDto;
 import com.backendteam5.finalproject.entity.Account;
-import com.backendteam5.finalproject.entity.AreaIndex;
 import com.backendteam5.finalproject.entity.UserRoleEnum;
 import com.backendteam5.finalproject.repository.AccountRepository;
-import com.backendteam5.finalproject.repository.AreaIndexRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
-
-
+@RequiredArgsConstructor
 @Service
 public class AccountService {
     private final BCryptPasswordEncoder passwordEncoder;
     private final AccountRepository accountRepository;
-    private final AreaIndexRepository areaIndexRepository;
     @Value("${manage.ADMIN_TOKEN}")
     private String ADMIN_TOKEN;
 
-    @Autowired
-    public AccountService(BCryptPasswordEncoder passwordEncoder, AccountRepository accountRepository,
-                          AreaIndexRepository areaIndexRepository){
-        this.passwordEncoder = passwordEncoder;
-        this.accountRepository = accountRepository;
-        this.areaIndexRepository = areaIndexRepository;
+    public void registerAccount(SignupRequestDto requestDto) {
+        validateAccountExistence(requestDto.getUsername());
+        UserRoleEnum role = getUserRole(requestDto);
+        accountRepository.save(createNewAccount(requestDto, role));
     }
 
-    public void registerAccount(SignupRequestDto requestDto) {
-        Optional<Account> account = accountRepository.findByUsername(requestDto.getUsername());
-        if (account.isPresent()) throw new IllegalArgumentException("중복된 사용자 ID가 존재합니다.");
-        UserRoleEnum role = UserRoleEnum.USER;
-        if (requestDto.isAdmin()) {
-            System.out.println("service" + ADMIN_TOKEN);
-            if (!requestDto.getAdminToken().equals(ADMIN_TOKEN)) {
-                throw new IllegalArgumentException("관리자 암호가 틀려 등록이 불가능합니다.");
-            }
-            role = UserRoleEnum.ADMIN;
+    private void validateAccountExistence(String username) {
+        if (accountRepository.existsByUsername(username)) {
+            throw new IllegalArgumentException("중복된 사용자 ID가 존재합니다.");
         }
+    }
 
-        accountRepository.findByUsername(requestDto.getUsername());
-        accountRepository.save(new Account(requestDto.getUsername(), passwordEncoder.encode(requestDto.getPassword()),
-                requestDto.getArea(), role));
+    private UserRoleEnum getUserRole(SignupRequestDto requestDto) {
+        if (requestDto.isAdmin()) {
+            validateAdminToken(requestDto.getAdminToken());
+            return UserRoleEnum.ADMIN;
+        }
+        return UserRoleEnum.USER;
+    }
 
+    private void validateAdminToken(String adminToken) {
+        if (!adminToken.equals(ADMIN_TOKEN)) {
+            throw new IllegalArgumentException("관리자 암호가 틀려 등록이 불가능합니다.");
+        }
+    }
+
+    private Account createNewAccount(SignupRequestDto requestDto, UserRoleEnum role) {
+        return new Account(requestDto.getUsername(),
+                passwordEncoder.encode(requestDto.getPassword()),
+                requestDto.getArea(), role);
     }
 }
